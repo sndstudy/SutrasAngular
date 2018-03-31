0626f82f77b7d8a7d5f687b8a62eb3de9cf04853
sndstudy <sanada2054tech@gmail.com>
2018-04-01 00:05:32 +0900

    新規コンポーネント追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  member = {
+    name:'山田太郎',
+    age:30
+  }
+
+  title:string = 'タイトル';
+  
+  msg:string = '<s>hogehoge</s>';
+
+  len:number = 3;
+
+  clazz:string = 'fore';
+
+  flag:boolean = true;
+
+  nowDate:string = '---';
+
+  show(e:any){
+    this.nowDate = new Date().toLocaleDateString();
+    console.log(e);
+  }
+
+  onClick1(){
+    console.log("outer");
+  }
+
+  onClick2(e:any){
+    e.stopPropagation();
+    console.log("inner");
+  }
+
+  txtMsg:string = "";
+
+  showTxt(input:string){
+    
+    this.txtMsg += `<li>${input}</li>`;
+
+  }
+
+  myName:string = "yamada";
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { Chapter4Component } from './chapter4/chapter4.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Chapter4Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,Chapter4Component]
 })
 export class AppModule { }
